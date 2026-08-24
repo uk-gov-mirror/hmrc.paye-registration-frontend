24322e2acab31003cabd1722e270d6900e698a40
arunankannan <103437165+arunankannan@users.noreply.github.com>
2026-08-24 15:29:18 +0100

    DL-20144 SCRS : paye-registration-frontend - Migrate play-frontend-hmrc to 13.1.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,15 +20,15 @@ import sbt.*
 private object AppDependencies {
 
   val playSuffix                      =  "-play-30"
-  val hmrcMongoVersion                =  "2.12.0"
+  val hmrcMongoVersion                =  "2.13.0"
   val taxYearVersion                  =  "6.0.0"
   val bootstrapVersion                =  "10.7.1"
   val playPartialsVersion             =  "10.2.0"
   val httpCachingVersion              =  "12.2.0"
   val playConditionalMappingVersion   =  "3.5.0"
-  val commonsValidatorVersion         =  "1.10.0"
+  val commonsValidatorVersion         =  "1.11.0"
   val scalaTestVersion                =  "3.2.12"
-  val playFrontendHmrc                =  "12.32.1"
+  val playFrontendHmrc                =  "13.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %%  s"bootstrap-frontend$playSuffix"    % bootstrapVersion,
@@ -43,13 +43,13 @@ private object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %%  s"bootstrap-test$playSuffix"        % bootstrapVersion          % Test,
-    "org.jsoup"               %   "jsoup"                             % "1.21.2"                  % Test,
-    "org.scalatestplus"       %%  "mockito-4-5"                       % s"$scalaTestVersion.0"    % Test,
+    "org.jsoup"               %   "jsoup"                             % "1.23.1"                  % Test,
+    "org.scalatestplus"       %%  "mockito-4-5"                       % "3.2.12.0"                % Test,
     "org.scalatestplus.play"  %%  "scalatestplus-play"                % "7.0.2"                   % Test,
-    "org.playframework"       %%  "play-test"                         % "3.0.9"                   % Test,
+    "org.playframework"       %%  "play-test"                         % "3.0.11"                   % Test,
     "com.vladsch.flexmark"    %   "flexmark-all"                      % "0.64.8"                  % Test,
     "org.scalatestplus"       %%  "scalacheck-1-17"                   % "3.2.18.0"                % Test,
-    "org.wiremock"            %   "wiremock-standalone"               % "3.13.1"                  % Test,
+    "org.wiremock"            %   "wiremock-standalone"               % "3.13.2"                  % Test,
     "uk.gov.hmrc.mongo"       %%  s"hmrc-mongo-test$playSuffix"       % hmrcMongoVersion          % Test
   )
 }
